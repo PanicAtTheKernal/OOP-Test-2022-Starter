471dc1d3b394aa8a789ac99febf2cb9359efdba0
unknown <drk3nlzux@relay.firefox.com>
2022-04-27 16:21:07 +0100

    Added the accessor methods and the toString method

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -22,4 +22,45 @@ public class Nematode {
         this(row.getString("name"), row.getInt("length"), row.getInt("limbs"), row.getString("gender"), row.getInt("eyes"));
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getLimbs() {
+        return limbs;
+    }
+
+    public void setLimbs(int limbs) {
+        this.limbs = limbs;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getEyes() {
+        return eyes;
+    }
+
+    public void setEyes(int eyes) {
+        this.eyes = eyes;
+    }
+    
+
 }
