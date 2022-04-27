ef16550c06c3db2a37feeb9070a1023a57c99114
unknown <drk3nlzux@relay.firefox.com>
2022-04-27 16:57:43 +0100

    Created a function to draw the body with and without limbs

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -9,6 +9,13 @@ import processing.data.TableRow;
 public class NematodeVisualiser extends PApplet
 {
 	ArrayList<Nematode> nematodes = new ArrayList<Nematode>();
+	int bodySize = 60;
+	int eyeSize = 15;
+	int femalePartSize = 40;
+	int malePartSize = 15;
+	int limbsSize = 35;
+	int nematodeColor = 255;
+	int nematodeWeigth = 5;
 
 	public void keyPressed()
 	{		
@@ -48,8 +55,38 @@ public class NematodeVisualiser extends PApplet
 		}
 	}
 
+	public void drawBottom(String gender, int limbsPresent) {
+		switch (gender) {
+			case "":
+				
+				break;
+		
+			default:
+				break;
+		}
+	}
+
+
+	public void drawBody(int limbsPresent) {
+		ellipseMode(CENTER);
+		stroke(nematodeColor);
+		strokeWeight(nematodeWeigth);
+		noFill();
+		ellipse(0, 0, bodySize, bodySize);
+		
+
+		if(limbsPresent == 1)
+		{
+			int x1 = -(bodySize/2);
+			int x2 = bodySize/2;
+			line(x1, 0, x1-limbsSize, 0);
+			line(x2, 0, x2+limbsSize, 0);
+		}
+	}
 
 	public void draw()
 	{	
+		translate(400, 400);
+		drawBody(0);
 	}
 }
